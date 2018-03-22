accab27a50e48e25a5d8148565ad6d335abed02d
mnjcqwi <mnjcqwi@gmail.com>
2018-03-21 20:36:37 -0400

    add annoatation

---
+++ src/main/java/com/mnjcqwi/concurrency/concurrency/annoatations/NotThreadSafe.java
@@ -0,0 +1,12 @@
+package com.mnjcqwi.concurrency.concurrency.annoatations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.Type)
+@Retention(RetentionPolicy.CLASS)
+public @interface NotThreadSafe {
+    String value() default "";
+}
